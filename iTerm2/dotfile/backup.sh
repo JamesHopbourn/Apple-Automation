ac9ba80a5f1accb70743b899109002b42d2a6a34
James Hopbourn <jameshopbourn@gmail.com>
2019-09-29 23:43:26 +0800

    back

--- iTerm2/dotfile/backup.sh
+++
@@ -1,8 +0,0 @@
-BackupDir=$HOME/.dotfile/AppList
-
-# All Apps
-ls -lh /Applications > ${BackupDir}/All_AppList
-
-/usr/local/bin/brew bundle dump --force --describe --file="${BackupDir}/Brewfile"
-
-brew deps --tree --installed > ${BackupDir}/deps.txt
