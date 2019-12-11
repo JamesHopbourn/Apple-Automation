d316fc3c25608ef6ec16044139514fba4a0da53d
James <james@JamesdeAir.lan>
2019-12-11 21:26:59 +0800

    delete line

--- Python/Clipboard to Reminders.py
+++ Python/Clipboard to Reminders.py
@@ -11,14 +11,12 @@ clipboard = pyperclip.paste()
 
 list = clipboard.split('\n')
 name = list[0]
-
 list.remove(list[0])
 body = '\n'.join(list)
 
 cmd = """osascript -e 'tell application "Reminders"
 	make new reminder at list "Drafts" with properties {name:"namehere", body:"bodyhere"}
 end tell'"""
-
 cmd = cmd.replace("namehere", name);
 cmd = cmd.replace("bodyhere", body);
 
