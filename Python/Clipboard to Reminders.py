6fc3eed8ef6c30fde902dc543aaabd3df5d4dc75
James <james@JamesdeAir.lan>
2019-12-11 21:15:24 +0800

    Clipboard to Reminders

---
+++ Python/Clipboard to Reminders.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+# @Time    : 2019-12-11
+# @Author  : James Hopbourn
+# @Email   : hopbourncontact@gmail.com
+# @File    : Clipboard to Reminders.py
+# @Software: Clipboard to Reminders
+
+import os
+import pyperclip
+clipboard = pyperclip.paste()
+
+list = clipboard.split('\n')
+name = list[0]
+
+list.remove(list[0])
+body = '\n'.join(list)
+
+cmd = """osascript -e 'tell application "Reminders"
+	make new reminder at list "Drafts" with properties {name:"namehere", body:"bodyhere"}
+end tell'"""
+
+cmd = cmd.replace("namehere", name);
+cmd = cmd.replace("bodyhere", body);
+
+def addreminders():
+     os.system(cmd)
+addreminders()
