d2b99239d1f84feb26aa3551e0a4452f380a11cc
James Hopbourn <hopbourncontact@gmail.com>
2019-12-12 23:58:42 +0800

    GitHub download.js

--- JavaScript Bookmarklet/GitHub download.js
+++ JavaScript Bookmarklet/GitHub download.js
@@ -1,1 +1,4 @@
-javascript:(function()%7B%20url%20=%20encodeURIComponent(window.location);%20url%20=%20url+'/archive/master.zip';%20window.open(url,'_self');%20%7D)();
+javascript:(function(){ 
+url = encodeURIComponent(window.location); url = url '/archive/master.zip';
+window.open(url,'_self');
+})();
