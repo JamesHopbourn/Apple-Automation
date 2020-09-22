9c935217d8b645172d4614a6458a310f1c900477
JamesHopbourn <jameshopbourn@gmail.com>
2020-09-22 11:42:32 +0800

    ✨ 沟通交流设置时间优先级

--- Drafts/TaskPaper.js
+++ Drafts/TaskPaper.js
@@ -1,5 +1,29 @@
 var text = draft.content;
 
+var chta = /@context\(\ud83d[\udcac].*\) @due\(today 19:10\)/g
+var chtb = /@context\(\ud83d[\udcac].*\) @due\(today 19:20\)/g
+var chtc = /@context\(\ud83d[\udcac].*\) @due\(today 19:30\)/g
+var chtd = /@context\(\ud83d[\udcac].*\) @due\(today 19:40\)/g
+var chat = /@context\(\ud83d[\udcac].*\) @due\(today 19:50\)/g
+
+var tchta = /@context\(\ud83d[\udcac].*\) @due\(tomorrow 19:10\)/g
+var tchtb = /@context\(\ud83d[\udcac].*\) @due\(tomorrow 19:20\)/g
+var tchtc = /@context\(\ud83d[\udcac].*\) @due\(tomorrow 19:30\)/g
+var tchtd = /@context\(\ud83d[\udcac].*\) @due\(tomorrow 19:40\)/g
+var tchat = /@context\(\ud83d[\udcac].*\) @due\(tomorrow 19:50\)/g
+
+if (text.match(chta) != null){ text = "1⃣️ " + text; }
+if (text.match(chtb) != null){ text = "2⃣️ " + text; }
+if (text.match(chtc) != null){ text = "3⃣️ " + text; }
+if (text.match(chtd) != null){ text = "4⃣️ " + text; }
+if (text.match(chat) != null){ text = "💬️ " + text; }
+
+if (text.match(tchta) != null){ text = "1⃣️ " + text; }
+if (text.match(tchtb) != null){ text = "2⃣️ " + text; }
+if (text.match(tchtc) != null){ text = "3⃣️ " + text; }
+if (text.match(tchtd) != null){ text = "4⃣️ " + text; }
+if (text.match(tchat) != null){ text = "💬️ " + text; }
+
 var folderTag = text.match(/@folder\(.+?\)/g);
 if (folderTag != null) {
 	var target = '/folder/' + folderTag[0].slice(8,-1);
