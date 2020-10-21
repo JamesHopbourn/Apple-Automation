cdcce48a7dd150df7be67d4eddba2783cfd72bdf
JamesHopbourn <jameshopbourn@gmail.com>
2020-10-21 17:06:17 +0800

    🐛 break line

--- JavaScript Bookmarklet/GitHub download.js
+++ JavaScript Bookmarklet/GitHub download.js
@@ -1,5 +1,5 @@
 javascript:(function(){ 
-url = encodeURIComponent(window.location); url = url '/archive/master.zip';
+url = window.location;
+url = url+'/archive/master.zip';
 window.open(url,'_self');
 })();
-
