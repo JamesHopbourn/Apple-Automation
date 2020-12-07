f29f9e2bc623c39f3044e9dd55060f070c82aa49
james <james@james.local>
2020-12-07 18:30:14 +0800

    ContenttoDrafts.js

---
+++ JavaScript Bookmarklet/ContenttoDrafts.js
@@ -0,0 +1,25 @@
+javascript: (function() {
+    function copyToClipboard(text) {
+        if (window.clipboardData && window.clipboardData.setData) {
+            return clipboardData.setData("Text", text);
+        } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");
+            } catch(ex) {
+                console.warn("Copy to clipboar failed.", ex);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
+    }
+    
+    copyToClipboard(document.body.innerText.replace(/[\r\n]{3,}/g, "\n\n"));
+    url = 'drafts4://x-callback-url/runAction?action=paste';
+    window.open(url,'_self');
+})();
