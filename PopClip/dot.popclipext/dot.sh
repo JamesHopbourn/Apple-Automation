2f2a1ae551847328c50dde6d9278caaf7415af71
JamesHopbourn <jameshopbourn@gmail.com>
2021-01-28 16:26:10 +0800

    dot.popclipext
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- PopClip/dot.popclipext/dot.sh
+++ PopClip/dot.popclipext/dot.sh
@@ -1,2 +1,2 @@
-result=$(echo "$POPCLIP_TEXT" | /usr/local/opt/gnu-sed/libexec/gnubin/sed '/^$/d ; s/^\([0-9]\|[0-9][0-9]\)、/\1. /g')
+result=$(echo "$POPCLIP_TEXT" | gsed '/^$/d ; s/^\([0-9]\|[0-9][0-9]\)、/\1. /g')
 /bin/echo -n "$result"
