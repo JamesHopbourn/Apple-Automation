496348667676eb3e19e9b8eb74dd370c3d98ec1d
JamesHopbourn <jameshopbourn@gmail.com>
2021-01-31 07:36:51 +0800

    笔记添加标签.js
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Drafts/笔记添加标签.js
+++ Drafts/笔记添加标签.js
@@ -2,31 +2,42 @@
 // 600 清单|400 兴趣/470 投资机会|400 兴趣/410 单车骑行|400 兴趣/405 绘图设计| 500 课余/001 政治体制|500 课余/004 课外知识|010 日记/020 恋爱日记|200 生活/250 恋爱知识|010 日记/011 生活日记|010 日记/013 社会时事|800 语录摘抄|900 演讲稿
 
 //// Script
-// 默认内容来自 Drafts
-var head = draft.processTemplate("[[title]]");
-var content = draft.processTemplate("[[body]]");
-
-// 如果 Drafts 没有内容从剪切板获取
-var text = draft.content;
-if (text == ''){
-  text = getClipboard()
-  line = text.split("\n")
-  head = line[0]
-  content = text.replace(line[0],'')
+// 默认内容来自文稿 否则从剪切板读取
+if (draft.content){
+  text = draft.content;
+}else{
+  text = getClipboard();
 }
 
-// 如果 content 首行为空删去
-if (content.split('\n')[0].length == 0){content = content.slice(1)}
-// 替换中文引号
-content = content.replace(/“/g,"「");
-content = content.replace(/”/g,"」");
-// 替换 \u00A0 空格
-content = content.replace(/[\u00A0]/g,'');
-// 当有两个换行合并为一个
-content = content.replace(/\n{2,}/g, '\n\n');
+// 微信公众号文章处理
+temp = text.split("\n");
+if(temp[temp.length-1].match(/mp.weixin.qq.com/g)){
+  head = temp;
+  // 删除发布日期
+  head = head.slice(0,1);
+  temp = temp.slice(3);
+  if (temp[0].length == 0)
+   temp = temp.slice(1);
+  // 删除收录分类
+  if (temp[0].match(/收录/))
+   temp = temp.slice(3);
+  if (temp[0].length == 0)
+   temp = temp.slice(1);
+  temp  = head.concat(temp);
+}
+head = temp[0];
+content = temp.slice(1).join("\n");
+
 // 删除行首行末空格
-head = head.replace(/^ +/gm, '');
-content = content.replace(/^ +/gm, '');
+head = head.trim();
+content = content.trim();
+// 替换中文引号和空格
+content = content.replace(/#/g,'');
+content = content.replace(/“/g,'「');
+content = content.replace(/”/g,'」');
+content = content.replace(/[\u00A0]/g,'');
+// 当有两个以上换行合并为两个
+content = content.replace(/\n{2,}/g, '\n\n');
 
 draft.defineTag("head", head);
 draft.defineTag("content", content);
