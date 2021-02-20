30060d43e3798ff793d8af6fd9304fe23667f224
JamesHopbourn <jameshopbourn@gmail.com>
2021-02-20 08:25:48 +0800

    list.sh
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- PopClip/list.popclipext/list.sh
+++ PopClip/list.popclipext/list.sh
@@ -1,2 +1,2 @@
-result=$(echo "$POPCLIP_TEXT"|gsed 's/^（.）/- / ; s/^.\( \|\.\|\、\|[0-9]\|，\|\)/- /; s/-  /- / ; /^-/! s/^/- /')
+result=$(printf "$POPCLIP_TEXT"|gsed 's/^（.*）/- / ; s/^\([0-9][0-9]\|[0-9]\|.\)\( \|\.\|\、\|，\|\)/- / ; s/^-  /- /')
 /bin/echo -n "$result"
