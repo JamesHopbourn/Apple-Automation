d41404f9ec5b3183a8e27a2464d010c2a19520f6
JamesHopbourn <jameshopbourn@gmail.com>
2021-02-24 11:24:26 +0800

    create date
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Drafts/笔记添加标签.js
+++ Drafts/笔记添加标签.js
@@ -21,6 +21,12 @@ if (tag == '300 学习/340 频道存档') {
   draft.defineTag('success', 'tg://msg?text='+encodeURI(text));
 }
 
+// create date
+var today = new Date();
+var cdate = today.getFullYear()+'年'+(today.getMonth()+1)+'月'+today.getDate()+'日';
+cdate = '  [复盘清单](bear://x-callback-url/open-note?id=B39C6145-4F8F-4E4E-935F-7DCF972EFD5A-45275-00009E975B3017E2&header=' + encodeURI(cdate) + ')'
+draft.defineTag('cdate', cdate);
+
 // 微信公众号文章处理
 temp = text.split('\n');
 if (temp[temp.length-1].match(/mp.weixin.qq.com/g)) {
@@ -33,23 +39,21 @@ if (temp[temp.length-1].match(/mp.weixin.qq.com/g)) {
   if (temp[0].match(/收录/)) temp = temp.slice(3);
   if (temp[0].length == 0)  temp = temp.slice(1);
   temp = head.concat(temp);
-  var today = new Date();
-  var cdate = today.getFullYear()+'年'+(today.getMonth()+1)+'月'+today.getDate()+'日';
-  cdate = '  [复盘清单](bear://x-callback-url/open-note?id=B39C6145-4F8F-4E4E-935F-7DCF972EFD5A-45275-00009E975B3017E2&header=' + encodeURI(cdate) + ')'
-  draft.defineTag('cdate', cdate);
-} else {
-  draft.defineTag('cdate', '');
 }
 head = temp[0];content = temp.slice(1).join('\n');
 
-// 删除行首行末空格
+// 标题文本符号替换删除
 head = head.trim();
+head = head.replace(/‘/g,'');
+head = head.replace(/’/g,'');
 head = head.replace(/#/g,'');
 head = head.replace(/“/g,'「');
 head = head.replace(/”/g,'」');
 head = head.replace(/[\u00A0]/g,'');
-// 替换中文引号 删除井号空格
+// 内容文本符号替换删除
 content = content.trim();
+content = content.replace(/‘/g,'');
+content = content.replace(/’/g,'');
 content = content.replace(/#/g,'');
 content = content.replace(/“/g,'「');
 content = content.replace(/”/g,'」');
