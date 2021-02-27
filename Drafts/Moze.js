35e26103dc62519b4c18ee16664c5b031b75bd20
JamesHopbourn <jameshopbourn@gmail.com>
2021-02-27 10:10:02 +0800

    Moze.js
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Drafts/Moze.js
+++ Drafts/Moze.js
@@ -1,4 +1,4 @@
-const food_list = [];
+const name_list = [];
 const text = draft.getTag('prompt_button');
 
 description = [
@@ -8,16 +8,16 @@ description = [
 ]
 
 for (i = 0; i < description.length; i++) {
-  food_list.push(description[i][0]);
+  name_list.push(description[i][0]);
 }
 
-draft.defineTag('name', description[food_list.indexOf(text)][0]);
-draft.defineTag('price', description[food_list.indexOf(text)][1]);
-draft.defineTag('category', description[food_list.indexOf(text)][2]);
+draft.defineTag('name', description[name_list.indexOf(text)][0]);
+draft.defineTag('price', description[name_list.indexOf(text)][1]);
+draft.defineTag('category', description[name_list.indexOf(text)][2]);
 
-// console.log(food_list)
+// console.log(name_list)
 // ==> [ '鸡排', '可乐', '葡萄糖' ]
-// console.log(food_list.indexOf('可乐')); 
+// console.log(name_list.indexOf('可乐')); 
 // ==> 1
-// console.log(description[food_list.indexOf('可乐')]);
+// console.log(description[name_list.indexOf('可乐')]);
 // ==> [ '可乐', 3, '饮料' ]
