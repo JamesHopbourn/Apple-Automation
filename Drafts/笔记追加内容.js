95d13898c74fe6b8a59bfd3ae299c4e0049cbbf8
JamesHopbourn <jameshopbourn@gmail.com>
2021-03-07 09:20:12 +0800

    符号组增添
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Drafts/笔记追加内容.js
+++ Drafts/笔记追加内容.js
@@ -19,7 +19,7 @@ text = text.replace(/\n{2,}/g, '\n\n');
 // 行末自动补全句号
 text = text.split('\n');
 for (var i = 0; i < text.length; i++){
-  if (text[i].length != 0 && text[i].charAt(text[i].length-1).match(/\.$|。$|！$|？$/) === null) {
+  if (text[i].length != 0 && text[i].charAt(text[i].length-1).match(/(\.|!|?|;|。|！|？|；)$/) === null) {
     if (text[i].charAt(text[i].length-1).match(/[a-zA-Z]/))
       text[i] += '.';
     else
