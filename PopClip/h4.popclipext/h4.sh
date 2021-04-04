ea7db833f65ceb467375ab13a52805e4ab85cee8
JamesHopbourn <jameshopbourn@gmail.com>
2021-04-05 06:02:38 +0800

    h4.sh
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- PopClip/h4.popclipext/h4.sh
+++ PopClip/h4.popclipext/h4.sh
@@ -1,2 +1,2 @@
-result=$(echo "$POPCLIP_TEXT"|gsed '1s/\*\*//g ; 1s/^[0-9]\/// ; 1s/^[0-9]、// ; 1s/^.*\.// ; 1s/$/@/ ; 1s/^/### / ; s/\*\*/::/g'|tr -d '\n'|tr '@' '\n')
-/bin/echo -n "$result"
+result=$(echo "$POPCLIP_TEXT"|gsed '1s/\(，\|：\)/ /g ; 1s/\*\*//g ; 1s/^[0-9]\/// ; 1s/^[0-9]、// ; 1s/^.*\.// ; 1s/$/@/ ; 1s/^/### / ; s/\*\*/::/g'|tr -d '\n'|tr '@' '\n')
+/bin/echo "$result"
