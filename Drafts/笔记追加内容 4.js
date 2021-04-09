07e24efbf50d50faae4dc64f876f3f857ed300f5
JamesHopbourn <jameshopbourn@gmail.com>
2021-04-09 09:46:13 +0800

    笔记追加内容 4.js
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Drafts/笔记追加内容 4.js
+++ Drafts/笔记追加内容 4.js
@@ -52,8 +52,8 @@ if (text.split(' ')[0] === 't' ||
   // 行末自动补全中西文句号
   text = text.split('\n');
   for (var i = 0; i < text.length; i++) {
-    if (text[i].length != 0 &&
-      text[i].charAt(text[i].length-1).match(/(\)|\.|!|\?|;|。|！|？|；)$/) === null) {
+    if (text[i].length != 0 && 
+       !text[i].charAt(text[i].length-1).match(/(\)|\.|!|\?|;|。|！|？|；)$/)) {
       if (text[i].charAt(text[i].length-1).match(/[0-9]/))
         continue;
       else if (text[i].charAt(text[i].length-1).match(/[a-zA-Z]/))
