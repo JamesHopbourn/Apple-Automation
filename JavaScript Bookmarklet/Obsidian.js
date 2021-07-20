00d1285cad451754d1a099ff81bd3a9f310d08e5
JamesHopbourn <jameshopbourn@gmail.com>
2021-07-20 12:28:27 +0800

    Obsidian.js
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- JavaScript Bookmarklet/Obsidian.js
+++ JavaScript Bookmarklet/Obsidian.js
@@ -1,4 +1,4 @@
 javascript: (function() {
-    url = 'obsidian://new?vault=Notes&name=' + document.title.replace(/( |)-.*$/,'').concat('.md') + '&content=' + document.body.innerText.replace(/\n/g, '%0A');
+    url = 'obsidian://new?vault=Notes&name=' + document.title.replace(/( |)-.*$/,'').concat('.md') + '&content=' + document.body.innerText.replace(/\n/g, '%0A').replace(/ /g, '%20');
     window.open(url,'_self');
 })();
