c7375c3d9199323d7fdec8ab4bfe33c53801dcbf
JamesHopbourn <jameshopbourn@gmail.com>
2021-09-27 13:29:34 +0800

    last_seen.py
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Python/last_seen.py
+++ Python/last_seen.py
@@ -1,10 +1,15 @@
+import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
-def gettime():
-  with open('last_seen.txt', 'r') as f:
-    return f.read()
+def last_seen():
+    conn = mysql.connector.connect(user='root', password='manager', host='localhost', database='Ubnt')
+    cursor = conn.cursor()
+    cursor.execute("select date,time from last_seen order by id DESC limit 1")
+    data = cursor.fetchone()
+    cursor.close()
+    return(str(data[0]) + ' ' + str(data[1]))
 
 if __name__ == '__main__':
   app.config['JSON_AS_ASCII'] = False
