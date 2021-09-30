693631e864306ccf59fc5db62901cbace9c734f5
JamesHopbourn <jameshopbourn@gmail.com>
2021-09-30 12:32:37 +0800

    last_seen.py
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Python/last_seen.py
+++ Python/last_seen.py
@@ -16,7 +16,7 @@ config = {
 def query():
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
-    cursor.execute("select concat(date,' ', time) from last_seen order by id DESC limit 1")
+    cursor.execute("select concat(date,' ',time) from last_seen order by id DESC limit 1")
     data = cursor.fetchone()
     cursor.close()
     return {"last_seen": str(data[0])}
@@ -25,14 +25,10 @@ def query():
 def query(date):
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
-    cursor.execute("select concat(date,' ',time) from last_seen where date = (%s)", (f'{date}',))
+    cursor.execute("SELECT IFNULL((SELECT CONCAT(date,' ',time) FROM last_seen WHERE date = (%s)) ,'未查询到相关记录')", (f'{date}',))
     data = cursor.fetchone()
     cursor.close()
-    if data:
-        data = str(data[0])
-    else:
-        data = '未查询到相关记录'
-    return {"last_seen": data}
+    return {"last_seen": str(data[0])}
 
 if __name__ == '__main__':
     uvicorn.run(app,host="0.0.0.0",port=80)
