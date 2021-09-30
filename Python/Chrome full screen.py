6af86ba97009ed35092b3deebeb6f6cb6547da76
JamesHopbourn <jameshopbourn@gmail.com>
2021-09-30 08:35:44 +0800

    Chrome full screen.py
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Python/Chrome full screen.py
+++ Python/Chrome full screen.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--kiosk")
-chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
+options = Options()
+options.add_argument("--kiosk")
+options.add_experimental_option("excludeSwitches", ['enable-automation'])
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get('https://localhost:3000')
+driver = webdriver.Chrome(options=options)
+driver.get('https://sspai.com')
