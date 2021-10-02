5c152e34f296f973abebf9a719d05fa584850824
JamesHopbourn <jameshopbourn@gmail.com>
2021-10-02 13:18:50 +0800

    Chrome full screen.py
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Python/Chrome full screen.py
+++ Python/Chrome full screen.py
@@ -3,7 +3,8 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--kiosk")
+options.add_experimental_option("detach", True)
 options.add_experimental_option("excludeSwitches", ['enable-automation'])
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options, executable_path='$HOME/dotfile/bin/chromedriver')
 driver.get('https://sspai.com')
