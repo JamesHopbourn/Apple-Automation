af09000b03dfee4bec7c22aa3f8fa1b06083881f
JamesHopbourn <jameshopbourn@gmail.com>
2022-01-17 01:24:39 +0800

    BIBnumberGenerator.py
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Python/BIBnumberGenerator.py
+++ Python/BIBnumberGenerator.py
@@ -1,5 +1,7 @@
 # 接力赛生成参赛号码
-for teamNum in range(1,34+1):
-	for memberNum in range(1,6+1):
+teamCount = 34
+memberCount = 6
+for teamNum in range(1,teamCount+1):
+	for memberNum in range(1,memberCount+1):
 		teamPrefix = '' if teamNum >= 10 else '0'
 		print(teamPrefix + str(teamNum) + '0' + str(memberNum))
