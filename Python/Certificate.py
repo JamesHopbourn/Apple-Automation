c39f5d9059187c44926be5e10b369ba2a6cb9c7b
JamesHopbourn <jameshopbourn@gmail.com>
2022-01-23 14:58:36 +0800

    Certificate.py
    Signed-off-by: JamesHopbourn <jameshopbourn@gmail.com>

--- Python/Certificate.py
+++ Python/Certificate.py
@@ -5,16 +5,20 @@ from PIL import ImageFont
 
 file = str(sys.argv[1])
 group = str(sys.argv[2])
-ranks = open(name, 'r')
+ranks = open(file, 'r')
 content = ranks.readlines()
 for text in content:
-	text = text.split(',')
-	print('正在生成' + text[0] + '的成绩证书...')
+	text = text.split('\t')
+	print('正在生成' + text[1] + '的成绩证书...')
 	img = Image.open('template.jpg')
 	I1 = ImageDraw.Draw(img)
-	Font = ImageFont.truetype('Songti.ttc', 40)
-	I1.text((382, 635), text[0].rstrip(), font=Font, fill=(0, 0, 0))
-	I1.text((382, 720), text[1], font=Font, fill=(0, 0, 0))
+	Font = ImageFont.truetype('PingFang.ttc', 40)
+	# 名字
+	I1.text((382, 635), text[1].rstrip(), font=Font, fill=(0, 0, 0))
+	# 参赛号码
+	I1.text((382, 720), text[0], font=Font, fill=(0, 0, 0))
+	# 枪响成绩
 	I1.text((382, 810), text[2], font=Font, fill=(0, 0, 0))
-	I1.text((382, 895), '第 ' + text[3].rstrip() + ' 名（' + group + '）', font=Font, fill=(0, 0, 0))
-	img.save(text[0] + ".png")
+	# 成绩排名
+	I1.text((382, 895), '第 ' + text[4].rstrip() + ' 名（' + group + '）', font=Font, fill=(0, 0, 0))
+	img.save(text[1] + ".png")
