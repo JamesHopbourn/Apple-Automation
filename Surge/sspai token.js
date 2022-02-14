6680b17db7d0ada1308925c19011c7d9907dd81f
JamesHopbourn <hopbourncontact@gmail.com>
2022-02-14 17:07:00 +0800

    Surge
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- Surge/sspai token.js
+++
@@ -1,5 +0,0 @@
-let cookies = $request.headers["Cookie"];
-cookies = cookies.match(/cross_token=.*?(?=;)/);
-cookies = cookies[0].replace(/cross_token=/, '');
-$persistentStore.write(cookies, "sspai_token");
-$done({});
