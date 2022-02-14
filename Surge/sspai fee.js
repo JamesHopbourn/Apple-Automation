ee49e8984f9ab4a41fffc7a81b7a15d41676c455
JamesHopbourn <hopbourncontact@gmail.com>
2022-02-14 16:43:42 +0800

    sspai fee.js
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- Surge/sspai fee.js
+++ Surge/sspai fee.js
@@ -11,14 +11,15 @@ function modify() {
 	}
 
 	$httpClient.get(req_data, function(error, response, data) {
-		let count = 0;
+		count = 0;
 		total = JSON.parse(data)["total"];
 		data = JSON.parse(data)["data"];
 		for (var i = 0; i < total; i++)
 			count += data[i]["fee"];
-		count = count.toString().replace(/^(.*)(..)$/, '$1.$2¥');
+		count = count.toString()
+		count = count.replace(/^(.*)(..)$/, '$1.$2¥');
 
-		let body = JSON.parse($response.body);
+		body = JSON.parse($response.body);
 		temp = body.data.followed_count;
 		text = temp + " · 本月稿费 " + count;
 		body.data.followed_count = text;
