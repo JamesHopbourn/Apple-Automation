6d102bbb21bbfe3d927a13ae741d0f13b364680b
JamesHopbourn <hopbourncontact@gmail.com>
2022-02-16 04:16:36 +0800

    ProcessOn.js
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

---
+++ ProcessOn.js
@@ -0,0 +1,25 @@
+var body = $response.body;
+data = body.match(/var definition = {[\s\S]*?};/)[0];
+data = data.replace(/(var definition = |;)/g, '');
+data = JSON.parse(data);
+
+var text = '';
+function dfs(node, n) {
+    if (!node || !node.children) {
+        return;
+    }
+    text += "\t".repeat(n) + "- " + node.title + "\n";
+    node.children.forEach(function(item, index) {
+        dfs(item, n + 1);
+    });
+    text = text.replace('<br>', '');
+    text = text.replace(/&nbsp/g, '');
+
+    return text;
+}
+
+let customURL = {
+    "url": "drafts://create?text=" + encodeURIComponent(dfs(data, 0))
+}
+$notification.post("", "点击添加到 Drafts", "", customURL);
+$done({});
