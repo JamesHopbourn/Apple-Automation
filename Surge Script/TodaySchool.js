0a9558189960248a957cf588b072424ca82c9ea0
James Hopbourn <hopbourncontact@gmail.com>
2022-04-04 03:20:02 +0800

    TodaySchool.js

--- Surge Script/TodaySchool.js
+++ Surge Script/TodaySchool.js
@@ -1,6 +1,4 @@
-let image = $request.url;
-let customURL = {
-    "url": "shortcuts://run-shortcut?name=" + encodeURI("保存查寝照片") + "&input=text&text=" + image
-}
-$notification.post("今日校园", "点击保存照片", "", customURL);
+$notification.post("今日校园", "点击保存照片", "", {
+    "url": "shortcuts://run-shortcut?name=" + encodeURI("保存查寝照片") + "&input=text&text=" + $request.url
+});
 $done({});
