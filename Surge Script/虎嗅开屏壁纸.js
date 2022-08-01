c0b5dea7248d0b0b8e2101f5c1660ec5563af35c
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-01 15:33:32 +0800

    虎嗅开屏壁纸.js
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- Surge Script/虎嗅开屏壁纸.js
+++ Surge Script/虎嗅开屏壁纸.js
@@ -1,9 +1,5 @@
-url = $request.url.match(/https:\/\/img.huxiucdn.com\/img\/hodor\/.*(?=\?)/g);
-
-if (url) {
-    $notification.post("虎嗅开屏壁纸", "点击保存照片", "", {
-        "url": `shortcuts://run-shortcut?name=${encodeURI("imgSave")}&input=text&text=${url[0]}`
-    });
-}
+$notification.post("虎嗅开屏壁纸", "点击保存照片", "", {
+    "url": `shortcuts://run-shortcut?name=${encodeURI("imgSave")}&input=text&text=${$request.url.slice(0, $request.url.indexOf('?'))}`
+});
 
 $done({});
