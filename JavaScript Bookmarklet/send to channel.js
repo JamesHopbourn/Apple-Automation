de7a7f8cd1ddadd2837f87402ae2a17440ea2310
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-26 18:05:58 +0800

    send to channel.js
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- JavaScript Bookmarklet/send to channel.js
+++ JavaScript Bookmarklet/send to channel.js
@@ -1,12 +1,13 @@
 javascript: void((function(doc) {
+    var result = prompt('学到了什么：', '');
     if (typeof jQuery == 'undefined') {
         var script_jQuery = document.createElement('script');
         script_jQuery.setAttribute('src', '//code.jquery.com/jquery-latest.min.js');
         document.body.appendChild(script_jQuery);
     }
-    text = `<a href='${$(location).attr('href')}'>${$(document).attr('title')}</a>`;
+    text = `<a href='${$(location).attr('href')}'>${$(document).attr('title')}</a>\n\n${result}`;
     $.get(`https://api.telegram.org/[API KEY]/sendMessage`, {
-        chat_id: '@[Public Channel Name]',
+        chat_id: '@[PUBLIC CHANNEL ID]',
         text: text,
         parse_mode: 'HTML'
     }, function(data, textStatus, jqXHR) {
