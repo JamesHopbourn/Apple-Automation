6b2e2f0c752c6f9230a908442393512811e4860c
James Hopbourn <jameshopbourn@gmail.com>
2023-01-13 21:16:52 +0800

    BilibiliTotalTime.js
    Signed-off-by: James Hopbourn <jameshopbourn@gmail.com>

--- JavaScript Bookmarklet/BilibiliTotalTime.js
+++ JavaScript Bookmarklet/BilibiliTotalTime.js
@@ -1,36 +1,36 @@
-javascript:(function(){
-var result = [];
-var content = document.getElementsByClassName('list-box')[0].querySelectorAll('li');
-for (var i = 0; i< content.length; i++){
-  temp = content[i].innerText.split('\n')[2];
-  if (temp.length < 6){
-    temp += ".00"
-  }
-  result.push(temp);
-}
+javascript: (function() {
+    var result = [];
+    var content = document.getElementsByClassName('list-box')[0].querySelectorAll('li');
+    for (var i = 0; i < content.length; i++) {
+        temp = content[i].innerText.split('\n')[2];
+        if (temp.length < 6) {
+            temp += ".00"
+        }
+        result.push(temp);
+    }
 
-var total = result.reduce((acc, time) => {
-  let parts = time.split(":");
-  let minutes = parseInt(parts[0], 10);
-  let seconds = parseFloat(parts[1]);
-  return acc + (minutes * 60 + seconds);
-}, 0);
+    var total = result.reduce((acc, time) => {
+        let parts = time.split(":");
+        let minutes = parseInt(parts[0], 10);
+        let seconds = parseFloat(parts[1]);
+        return acc + (minutes * 60 + seconds);
+    }, 0);
 
-let hours = Math.floor(total / 3600);
-let minutes = Math.floor((total % 3600) / 60);
-let seconds = total % 60;
+    let hours = Math.floor(total / 3600);
+    let minutes = Math.floor((total % 3600) / 60);
+    let seconds = total % 60;
 
-var textArea = document.createElement("textarea");
-hours = hours.toString().padStart(2, "0");
-minutes = minutes.toString().padStart(2, "0");
-textArea.value = hours + ":" + minutes;
-document.body.appendChild(textArea);
+    var textArea = document.createElement("textarea");
+    hours = hours.toString().padStart(2, "0");
+    minutes = minutes.toString().padStart(2, "0");
+    textArea.value = hours + ":" + minutes;
+    document.body.appendChild(textArea);
 
-textArea.select();
+    textArea.select();
 
-document.execCommand("copy");
+    document.execCommand("copy");
 
-document.body.removeChild(textArea);
+    document.body.removeChild(textArea);
 
-console.log("Text copied to clipboard");
+    console.log("Text copied to clipboard");
 })();
